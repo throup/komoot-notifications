fafcd8b80a086fd10809f86c241fd5b531c4ed99
Chris Throup <chris@throup.eu>
2022-06-25 14:53:53 +0200

    Hello world server

---
+++ src/main/scala/eu/throup/komoot/Main.scala
@@ -0,0 +1,9 @@
+package eu.throup
+package komoot
+
+import cats.effect.{ExitCode, IO, IOApp}
+
+object Main extends IOApp {
+  def run(args: List[String]): IO[ExitCode] =
+    Server.stream[IO].compile.drain.as(ExitCode.Success)
+}
---
+++ src/main/scala/eu/throup/komoot/Server.scala
@@ -0,0 +1,33 @@
+package eu.throup
+package komoot
+
+import cats.effect.{Async, Resource}
+import cats.syntax.all._
+import com.comcast.ip4s._
+import fs2.Stream
+import org.http4s.ember.client.EmberClientBuilder
+import org.http4s.ember.server.EmberServerBuilder
+import org.http4s.implicits._
+import org.http4s.HttpRoutes
+import org.http4s.HttpApp
+import org.http4s.server.middleware.Logger
+
+object Server {
+  def stream[F[_]: Async]: Stream[F, Nothing] = {
+    val routes: HttpRoutes[F]    = Routes.routes[F]
+    val httpApp: HttpApp[F]      = routes.orNotFound
+    val finalHttpApp: HttpApp[F] = Logger.httpApp(true, true)(httpApp)
+
+    for {
+      exitCode <- Stream.resource(
+                    EmberServerBuilder
+                      .default[F]
+                      .withHost(ipv4"0.0.0.0")
+                      .withPort(port"8080")
+                      .withHttpApp(finalHttpApp)
+                      .build >>
+                      Resource.eval(Async[F].never)
+                  )
+    } yield exitCode
+  }.drain
+}
